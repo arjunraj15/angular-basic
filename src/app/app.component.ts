bff74606bb1738ef1623d2f05dc215f1279d96b6
arjunraj15 <arjunrajvcet@gmail.com>
2019-09-15 18:02:13 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server',name:'testserver',content:'sample test case'}];  
+  serverElements = [{ type1:'server',name:'testserver',content:'sample test case'}];  
 
   
 }
