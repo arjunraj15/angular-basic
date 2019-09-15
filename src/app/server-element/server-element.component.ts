38bfa32b8835a09705e6fc87dbca500d96612f38
arjunraj15 <arjunrajvcet@gmail.com>
2019-09-15 18:00:19 +0530

    2 files modified

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
- @Input() element :{type:string, name:string , content:string}
-  constructor() { }
+ @Input() element : {type:string, name:string , content:string}
+ constructor() { }
 
   ngOnInit() {
   }
